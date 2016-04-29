36b406d37bcbbd8678c726823c399c1010234b1c
escribis <escribis@users.noreply.github.com>
2016-04-29 22:33:34 +0200

    ajoute '_', 'zone-combats','static' dans le path

--- .infra/docs/conf.py
+++ .infra/docs/conf.py
@@ -213,7 +213,10 @@ html_title = "Meylan"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = [os.path.join(CONF_DIR, 'static')]
+html_static_path = [
+    os.path.join(CONF_DIR, 'static'),
+    os.path.join(PROJECT_DIR, '_', 'zone-combats','static'),
+    ]
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
